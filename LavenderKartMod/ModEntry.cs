de5c11e303ba5628069a6fe821569d99367d2a46
RoseBunnyPuppy <rosebunnypuppy@proton.me>
2026-09-11 11:56:58 -0700

    Created ModEntry, added asset, created manifest

---
+++ LavenderKartMod/ModEntry.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+
+namespace LavenderKartMod
+{
+    internal sealed class ModEntry : Mod
+    {
+        /// <summary>The mod entry point, called after the mod is first loaded.</summary>
+        /// <param name="helper">Provides simplified APIs for writing mods.</param>
+        public override void Entry(IModHelper helper)
+        {
+            helper.Events.Content.AssetRequested += OnAssetRequested; ;
+        }
+
+        private void OnAssetRequested(object? sender, AssetRequestedEventArgs e)
+        {
+            // skip if not the MineCart spritesheet
+            if (!e.NameWithoutLocale.IsEquivalentTo("Minigames/MineCart")) return;
+
+            // replace with our spritesheet
+            e.LoadFromModFile<Texture2D>(
+                "assets/LavenderKart.png",
+                AssetLoadPriority.Exclusive
+            );
+        }
+    }
+}
